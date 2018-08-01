5db2a5f441484c79fc749f4ad3e064d6fb474f90
Bogdan Nedelcu <nedbog@gmail.com>
2018-08-01 14:48:46 +0300

    finishing project.

---
+++ GetDataFromWebsite/TerminalApp/HtmlDocumentUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace TerminalApp
+{
+    public class HtmlDocumentUtility
+    {
+        public static string GetHtmlContent(string url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    return client.DownloadString(url);
+                }
+            }
+
+            return "";
+        }
+    }
+}
--- GetDataFromWebsite/TerminalApp/Program.cs
+++ GetDataFromWebsite/TerminalApp/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Microsoft.Win32;
 
 namespace TerminalApp
 {
@@ -22,24 +25,85 @@ namespace TerminalApp
             Console.ReadLine();
         }
 
-        async static void GetRequestAsync(string url, string cim)
+        static void GetRequestAsync(string url, string cim)
         {
-            string html = string.Empty;
-
             string searchUrl = url + "?cim=" + cim;
-            using (HttpClient client = new HttpClient())
-            using (HttpResponseMessage response = await client.GetAsync(searchUrl))
-            using (HttpContent content = response.Content)
+
+            var htmlContent = HtmlDocumentUtility.GetHtmlContent(searchUrl);
+
+            if (!string.IsNullOrEmpty(htmlContent))
             {
-                _htmlContent = await content.ReadAsStringAsync();
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(htmlContent);
 
-                if (_htmlContent == null)
+                var drugDetails = doc.DocumentNode.SelectNodes("//button[@data-target='#detailsModal']");
+
+                if (drugDetails.Count == 1 && drugDetails[0].InnerHtml.ToLower() == "detalii")
                 {
-                    _htmlContent = string.Empty;
-                }
+                    var rcp = drugDetails[0].GetAttributeValue("data-linkrcp", "not found");
+                    var prospect = drugDetails[0].GetAttributeValue("data-linkpro", "not found");
+                    //Console.WriteLine($"RCP: {rcp}\nProspect: {prospect}");
+                    if (rcp != "not found" || prospect != "not found")
+                    {
+                        Console.WriteLine(rcp);
+                        Console.WriteLine(prospect);
+                    }
 
-                Console.WriteLine(_htmlContent);
+                    Process.Start(rcp);
+                    Process.Start(prospect);
+
+                }
             }
+
+        }
+        static bool IsAcrobatReaderInstalled()
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey _mainKey = Microsoft.Win32.Registry.CurrentUser;
+                _mainKey = _mainKey.OpenSubKey(@"Software\Adobe\Acrobat Reader");
+                if (_mainKey == null)
+                {
+                    _mainKey = Microsoft.Win32.Registry.LocalMachine;
+                    _mainKey = _mainKey.OpenSubKey(@"Software\Adobe\Acrobat Reader");
+                    if (_mainKey == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        static string GetDefaultBrowserPath()
+        {
+
+            string key = @"htmlfile\shell\open\command";
+            RegistryKey registryKey =
+            Registry.ClassesRoot.OpenSubKey(key, false);
+            // get default browser path
+
+            return ((string)registryKey.GetValue(null, null)).Split('"')[1];
+
+        }
+
+        static void WriteHtmlToDisk(string html)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"\index.html";
+
+            File.WriteAllText(path, html);
         }
     }
 }
